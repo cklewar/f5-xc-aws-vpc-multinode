6343ec4dd9e9335192979aac9a0ce727047e7ae4
cklewar@t-online.de <cklewar@t-online.de>
2022-07-26 11:13:13 +0200

    init

---
+++ versions.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.2.3"
+
+  required_providers {
+    f5xc = {
+      source  = "volterraedge/volterra"
+      version = ">= 0.11.9"
+    }
+
+    local = ">= 2.2.3"
+    null  = ">= 3.1.1"
+  }
+}
