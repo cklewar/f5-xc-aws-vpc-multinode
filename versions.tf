a002ca43e7da678d92ccd355b4298866eaa83ad5
cklewar <cklewar@t-online.de>
2023-01-27 01:13:38 +0100

    0.11.18

--- versions.tf
+++ versions.tf
@@ -8,19 +8,17 @@ terraform {
       name = "f5-xc-aws-vpc-module"
     }
   }
-
+  
   required_providers {
     volterra = {
-      source  = "volterraedge/volterra"
-      version = ">= 0.11.14"
+      source = "volterraedge/volterra"
+      version = "= 0.11.18"
     }
-
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
       version = ">= 4.33.0"
     }
-
     local = ">= 2.2.3"
-    null  = ">= 3.1.1"
+    null = ">= 3.1.1"
   }
 }
